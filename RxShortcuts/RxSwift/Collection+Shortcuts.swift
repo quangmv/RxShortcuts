7b866217a6e14e392a55b855d784fcad5cdd17e5
Quang Mai Van <quang.app@gmail.com>
2020-07-30 22:20:53 +0700

    Update Collection+Shortcuts.swift

--- RxShortcuts/RxSwift/Collection+Shortcuts.swift
+++ RxShortcuts/RxSwift/Collection+Shortcuts.swift
@@ -17,32 +17,8 @@ public extension Collection where Iterator.Element: ObservableType {
      
      - returns: The observable sequence that merges the elements of the observable sequences.
      */
-    func merge() -> Observable<Self.Iterator.Element.E> {
+    func merge() -> Observable<Self.Iterator.Element.Element> {
         return Observable.from(self).merge()
-    }
-
-    /**
-     Merges the specified observable sequences into one observable sequence by using the selector function whenever any of the observable sequences produces an element.
-     
-     - seealso: [combineLatest operator on reactivex.io](http://reactivex.io/documentation/operators/combinelatest.html)
-     
-     - parameter resultSelector: Function to invoke whenever any of the sources produces an element.
-     - returns: An observable sequence containing the result of combining elements of the sources using the specified result selector function.
-     */    
-    func combineLatest<T>(resultSelector: @escaping ([Self.Iterator.Element.E]) throws -> T) -> Observable<T> {
-        return Observable.combineLatest(self, resultSelector)
-    }
-    
-    /**
-     Merges the specified observable sequences into one observable sequence by using the selector function whenever all of the observable sequences have produced an element at a corresponding index.
-     
-     - seealso: [zip operator on reactivex.io](http://reactivex.io/documentation/operators/zip.html)
-     
-     - parameter resultSelector: Function to invoke for each series of elements at corresponding indexes in the sources.
-     - returns: An observable sequence containing the result of combining elements of the sources using the specified result selector function.
-     */
-    func zip<T>(resultSelector: @escaping ([Self.Iterator.Element.E]) throws -> T) -> Observable<T> {
-        return Observable.zip(self, resultSelector)
     }
     
     /**
@@ -52,7 +28,7 @@ public extension Collection where Iterator.Element: ObservableType {
      
      - returns: An observable sequence that contains the elements of each observed inner sequence, in sequential order.
      */
-    func concat() -> Observable<Self.Iterator.Element.E> {
+    func concat() -> Observable<Self.Iterator.Element.Element> {
         return Observable.from(self).concat()
     }
 }
